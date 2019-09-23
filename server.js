996f2f3d56b887682d2dda1e78802487e2b5eb5c
Dwight Millar <millar.dwight@gmail.com>
2019-09-23 16:02:14 -0700

    parameterized queries

--- server.js
+++ server.js
@@ -19,161 +19,181 @@ const staticMiddlewareFunction = express.static(path.join(htmlDirectory));
 
 server.use(staticMiddlewareFunction);
 
-server.get('/api/getclasses', function (request, response) {
-  db.connect(function () {
-    const id = request._parsedUrl.query;
-    if(id){
-      var query = "SELECT * FROM `classes` WHERE " + id;
-    } else {
-      query = "SELECT * FROM `classes`";
-    }
-    db.query(query, function (error, data, fields) {
-      if (!error) {
+server.get('/api/getclasses', function (request, response, next) {
+  let params = [];
+
+  if (request._parsedUrl.query){
+    params.push(request._parsedUrl.query);
+    var query = `SELECT * FROM 'classes' WHERE ?`;
+
+  } else {
+    query = `SELECT * FROM 'classes'`;
+  }
+  db.query(query, params, function (error, data, fields) {
+    if (error) return next(error);
+    response.send({
+        success: true,
+        data
+    });
+  });
+});
+
+
+server.get('/api/getstudents', function (request, response, next) {
+  let params = [];
+
+  params.push(request._parsedUrl.query);
+
+  const query = `SELECT * FROM 'students' WHERE ?`;
+
+  db.query(query, params, function (error, data, fields) {
+  if (error) return next(error);
+    response.send({
+      success: true,
+      data
+    });
+  });
+});
+
+server.get('/api/getassignments', function (request, response, next) {
+    let params = [];
+
+    params.push(request._parsedUrl.query);
+
+    const query = `SELECT * FROM 'assignments' WHERE ?`;
+
+    db.query(query, params, function (error, data, fields) {
+      if (error) return next(error);
         response.send({
           success: true,
           data
         });
-      }
     });
-  })
 });
 
+server.post('/api/addstudent', function (request, response, next) {
+    let params = [];
 
-server.get('/api/getstudents', function (request, response) {
-  db.connect(function () {
-    const id = request._parsedUrl.query;
-    const query = "SELECT * FROM `students` WHERE " + id;
-    db.query(query, function (error, data, fields) {
-      if (!error) {
+    params.push(request.body.name);
+    params.push(request.body['class_id']);
+
+    const query = `INSERT INTO students (name, class_id) VALUES (\"?\",?)`;
+    db.query(query, params, function (error, data, fields) {
+      if (error) return next(error);
         response.send({
           success: true,
           data
         });
-      }
     });
-  })
 });
 
-server.get('/api/getassignments', function (request, response) {
-  db.connect(function () {
-    const id = request._parsedUrl.query;
-    const query = "SELECT * FROM `assignments` WHERE " + id;
-    db.query(query, function (error, data, fields) {
-      if (!error) {
+server.delete('/api/deletestudent', function (request, response, next) {
+    let params = [];
+
+    params.push(request.body.id);
+
+    var query = `DELETE students, assignments FROM students INNER JOIN assignments ON students.id = assignments.student_id WHERE students.id = ?`;
+
+    db.query(query, params, function (error, data, fields) {
+      if (error) return next(error);
         response.send({
           success: true,
           data
         });
-      }
     });
-  })
 });
 
-server.post('/api/addstudent', function (request, response) {
-  db.connect(function () {
-    const query = "INSERT INTO students (name, class_id) VALUES (\"" + request.body.name + "\"," + request.body['class_id'] + ")";
-    db.query(query, function (error, data, fields) {
-      if (!error) {
-        response.send({
-          success: true,
-          data
-        });
-      }
-    });
-  })
-});
+server.delete('/api/deleteclass', function (request, response, next) {
+    let params = [];
 
-server.delete('/api/deletestudent', function (request, response) {
-  db.connect(function () {
-    var query = "DELETE students, assignments FROM students INNER JOIN assignments ON students.id = assignments.student_id WHERE students.id = " + request.body.id;
+    params.push(request.body.id);
 
-    db.query(query, function (error, data, fields) {
-      if (!error) {
-        response.send({
-          success: true,
-          data
-        });
-      }
-    });
-  })
-});
-
-server.delete('/api/deleteclass', function (request, response) {
-  db.connect(function () {
     var query = `DELETE classes, students, assignments
                   FROM classes
                   LEFT JOIN students on classes.id = students.class_id
                   LEFT JOIN assignments on assignments.class_id = students.class_id
-                  WHERE classes.id = ${request.body.id}`
+                  WHERE classes.id = ?`
+
     console.log(query);
-    db.query(query, function (error, data, fields) {
-      if (!error) {
+    db.query(query, params, function (error, data, fields) {
+      if (error) return next(error);
         response.send({
           success: true,
           data
         });
-      }
     });
-  })
 });
 
-server.post('/api/addassignment', function (request, response) {
-  db.connect(function () {
+server.post('/api/addassignment', function (request, response, next) {
+    let params = [];
+
+    params.push(request.body.scores);
+
     const query = ` INSERT INTO assignments
                       (title, score, totalpoints, student_id, class_id)
                     VALUES
-                      ${request.body.scores}`;
-    db.query(query, function (error, data, fields) {
-      if (!error) {
+                      ?`;
+    db.query(query, params, function (error, data, fields) {
+      if (error) return next(error);
         response.send({
           success: true,
           data
         });
-      }
     });
-  })
 });
 
-server.post('/api/addclass', function (request, response) {
-  db.connect(function () {
+server.post('/api/addclass', function (request, response, next) {
     const title = JSON.stringify(request.body.name);
-    const query = `INSERT INTO classes(title) VALUES (${title})`;
-    db.query(query, function (error, data, fields) {
-      if (!error) {
+    let params = [];
+
+    params.push(title);
+
+    const query = `INSERT INTO classes(title) VALUES (?)`;
+    db.query(query, params, function (error, data, fields) {
+      if (error) return next(error);
         response.send({
           success: true,
           data
         });
-      }
     });
-  })
 });
 
-server.patch('/api/updatescore', function (request, response) {
-  db.connect(function () {
+
+
+server.patch('/api/updatescore', function (request, response, next) {
     const scores = request.body.scores;
     let formattedScores = '';
     let affectedIDs = '';
+    let params = [];
+
+
 
     for (let assignmentID in scores) {
-     formattedScores += `WHEN ${assignmentID} THEN ${scores[assignmentID]} `;
-     affectedIDs += `${assignmentID}, `;
+      formattedScores += `WHEN ${assignmentID} THEN ${scores[assignmentID]} `;
+      affectedIDs += `${assignmentID}, `;
     }
-
     affectedIDs = affectedIDs.slice(0, -2);
 
-    const query = `UPDATE assignments SET score = CASE id ${formattedScores} END WHERE id IN (${affectedIDs})`;
 
-    db.query(query, function (error, data, fields) {
-      if(!error) {
+
+    params.push(formattedScores, affectedIDs);
+    const query = `UPDATE assignments SET score = CASE id ? END WHERE id IN (?)`;
+
+
+    db.query(query, params, function (error, data, fields) {
+      if(error) return next(error);
         response.send({
           success: true,
           data
         })
-      }
     })
-  })
 })
 
+server.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).json({
+    success: false
+  });
+})
 
 server.listen(3001, function () { console.log('server is listening on port 3001') });
