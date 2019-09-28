70477525f012d7fe84f18a22a92b5fb9ffc885af
Dwight Millar <millar.dwight@gmail.com>
2019-09-28 11:55:48 -0700

    removed mysql credentials from repo

--- mysql_credentials.js
+++
@@ -1,9 +0,0 @@
-const credentials = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'root',
-    'database': 'class_manager',
-    'port': 3306
-};
-
-module.exports = credentials;
