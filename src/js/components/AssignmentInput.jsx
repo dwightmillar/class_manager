c076e6e31615046ba568f2b4cb4f3a620ff61381
Dwight Millar <millar.dwight@gmail.com>
2019-10-21 15:36:50 -0700

    added limit to max points in assignment input

--- src/js/components/AssignmentInput.jsx
+++ src/js/components/AssignmentInput.jsx
@@ -119,6 +119,8 @@ export default class Assignment extends React.Component {
   handleMaxPointsInput(event) {
     if (isNaN(event.target.value)) {
       return false;
+    } else if (event.target.value > 999) {
+      return false;
     }
     this.setState({ 'maxPoints': event.target.value })
   }
@@ -137,7 +139,7 @@ export default class Assignment extends React.Component {
       studentScore = 0;
     } else if (!isNaN(event.target.value)){
       studentScore = parseInt(event.target.value);
-    } else if (event.target.value > 999) {
+    } else if (event.target.value.length > 3) {
         return false;
     }
 
